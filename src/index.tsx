4eb815570ef1801fcb8a532a08718d903a20f3f4
Andras Eisenberger <Andras.Eisenberger@travelchapter.com>
2020-06-11 14:22:35 +0100

    Ref timing basic example

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appRoot = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface ParentState {
+  show: boolean;
+  text: string;
+}
+
+class Parent extends React.Component<{}, ParentState> {
+  containerRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props = {}) {
+    super(props);
+    this.state = { show: false, text: "Init" };
+    this.containerRef = React.createRef();
+  }
+
+  handleClick = (): void => {
+    this.setState((state) => ({
+      show: !state.show,
+    }));
+  };
+
+  onChildMount = (): void => {
+    if (this.containerRef.current) {
+      this.setState({
+        text: "Has ref!",
+      });
+    } else {
+      this.setState({
+        text: "No ref!",
+      });
+    }
+  };
+
+  render() {
+    return (
+        <div>
+          <button onClick={this.handleClick}>Show modal</button>
+          {this.state.show ? (
+              <div ref={this.containerRef}>
+                <Child text={this.state.text} onLoad={this.onChildMount} />
+              </div>
+          ) : (
+              <div>No show!</div>
+          )}
+        </div>
+    );
+  }
+}
+
+interface ChildProps {
+  text: string;
+  onLoad: () => void;
+}
+
+class Child extends React.Component<ChildProps> {
+  componentDidMount(): void {
+    this.props.onLoad();
+  }
+
+  render(): React.ReactNode {
+    return <span>{this.props.text}</span>;
+  }
+}
+
+ReactDOM.render(<Parent />, appRoot);
